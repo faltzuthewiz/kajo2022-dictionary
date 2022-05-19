b57298bd63140ac0854abddf0058112b186dc8bf
Sonja Fallstrom <jawnlemon@gmail.com>
2022-05-19 14:34:54 +0300

    huge mess

--- sanakirja.js
+++ sanakirja.js
@@ -174,6 +174,58 @@ function search2() {
   }
 
   
+let finEng = document.getElementById("suomi-englanti");
+let finSwe = document.getElementById("suomi-ruotsi");
+
+// Create options
+let finOption1 = document.getElementById("suomi1").value;
+let sweOption1 = document.getElementById("ruotsi1").value;
+let engOption1 = document.getElementById("englanti1").value;
+
+let finOption2 = document.getElementById("suomi2").value;
+let sweOption2 = document.getElementById("ruotsi2").value;
+let engOption2 = document.getElementById("englanti2").value;
+console.log(finOption1);
+
+// create arrays for both options
+let options1 = [finOption1, sweOption1, engOption1];
+let options2 = [finOption2, sweOption2, engOption2];
+
+
+// Trying out a new way to get the selected value.
+/*
+const btn = document.querySelector('#btn');
+        const sb = document.querySelector('#lang1')
+        btn.onclick = (event) => {
+            event.preventDefault();
+            // show the selected index
+            alert(sb.value);
+        };
+*/
+
+// Test selection
+function testSelection() {
+var e = document.getElementById("lang1");
+var selected1 = e.value;
+console.log(selected1);
+
+    if (selected1 === "suomi1") {
+        selected1 = words[i].suomi;
+    }
+    console.log(selected1);
+}
+
+
+// My own code once again.
+function selectFirstLanguage() {
+    for (var i = 0; i < options1.length; i++) {
+        if (options1[i].checked == true) {
+            console.log(options1[i]);
+            return options1[i];
+        }
+    }
+}
+
 // Selects two languages to display
   function selectLanguages() {
     let text = "<table><tr><th>suomi</th><th>englanti</th></tr>";
@@ -182,12 +234,31 @@ function search2() {
     }
     text += "</table>"
     document.getElementById("outputAreaGetAll").innerHTML = text;
+
+    // Uncheck other radio button options
+    finSwe.checked = false;
+    finEng.checked = false;
     
    // console.log(selectedLanguage1);
   }
 
+  function selectLanguagesFinSwe() {
+    let text = "<table><tr><th>suomi</th><th>ruotsi</th></tr>";
+    for (var i = 0; i < words.length; i++) {
+        text += "<tr><td>" + words[i].suomi + "</td><td> " + words[i].svenska + "</td></tr>";
+    }
+    text += "</table>"
+    document.getElementById("outputAreaGetAll").innerHTML = text;
+    
+    finSwe.checked = false;
+    finEng.checked = false;
+   // console.log(selectedLanguage1);
+  }
+
   function checkSelection() {
-      if (document.getElementById("suomi-englanti").checked == true) {
+      if (finEng.checked == true) {
         return selectLanguages();
+  } else if (finSwe.checked == true) {
+        return selectLanguagesFinSwe();
   }
 }
