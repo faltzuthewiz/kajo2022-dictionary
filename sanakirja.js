1d02ddcde06e132fbf5d57198592fd023b269b84
Sonja Fallstrom <jawnlemon@gmail.com>
2022-05-30 14:01:53 +0300

    did I change something?

--- sanakirja.js
+++ sanakirja.js
@@ -154,7 +154,7 @@ var words =
     }
 ];
 
-// Function that retrieves all the words in the dictionary. (Selected language: Finnish - others)
+// Function that retrieves all the words in the dictionary.
 function getAll() {
     let text = "<table><tr><th>suomi</th><th>ruotsi</th><th>englanti</th></tr>";  // creates a table.
 for (var i = 0; i < words.length; i++) {
@@ -175,54 +175,6 @@ function search2() {
   }
   */
 
-// Create variables for different language options.
-let finEng = document.getElementById("suomi-englanti");
-let finSwe = document.getElementById("suomi-ruotsi");
-let engFin = document.getElementById("englanti-suomi");
-let sweFin = document.getElementById("ruotsi-suomi");
-let engSwe = document.getElementById("englanti-ruotsi");
-let sweEng = document.getElementById("ruotsi-englanti");
-
-
-// Selects two languages to display
-  function selectLanguages() {
-    let text = "<table><tr><th>suomi</th><th>englanti</th></tr>";
-    for (var i = 0; i < words.length; i++) {
-        text += "<tr><td>" + words[i].suomi + "</td><td> " + words[i].english + "</td></tr>";
-    }
-    text += "</table>"
-    document.getElementById("outputAreaGetAll").innerHTML = text;
-
-    // Uncheck other radio button options
-    uncheckOptions();
-    
-   // console.log(selectedLanguage1);
-  }
-
-  function selectLanguagesFinSwe() {
-    let text = "<table><tr><th>suomi</th><th>ruotsi</th></tr>";
-    for (var i = 0; i < words.length; i++) {
-        text += "<tr><td>" + words[i].suomi + "</td><td> " + words[i].svenska + "</td></tr>";
-    }
-    text += "</table>"
-    document.getElementById("outputAreaGetAll").innerHTML = text;
-    
-    uncheckOptions();
-   // console.log(selectedLanguage1);
-  }
-
-  function selectLanguagesEngFin() {
-    let text = "<table><tr><th>englanti</th><th>suomi</th></tr>";
-    for (var i = 0; i < words.length; i++) {
-        text += "<tr><td>" + words[i].english + "</td><td> " + words[i].suomi + "</td></tr>";
-    }
-    text += "</table>"
-    document.getElementById("outputAreaGetAll").innerHTML = text;
-    
-    uncheckOptions();
-   // console.log(selectedLanguage1);
-  }
-
   let lang1 = [];
   let lang2 = [];
   let header1 = "";
@@ -230,6 +182,9 @@ let sweEng = document.getElementById("ruotsi-englanti");
   let suomi = [];
   let english = [];
   let svenska = [];
+  let suomiheader = "suomi";
+  let englishheader = "englanti";
+  let svenskaheader = "ruotsi";
 
   
   for (var i = 0; i < words.length; i++) {
@@ -243,74 +198,13 @@ let sweEng = document.getElementById("ruotsi-englanti");
     svenska = svenska.split("<br>");
     
 
- // Starts when the user clicks one of the language options. Checks which two languages was selected in which order.
- /* Don't think I need this long since I dropped the radio buttons.
-  function checkSelection() {
-      if (finEng.checked == true) {
-        lang1 = suomi;
-        lang2 = english;
-        header1 = "suomi";
-        header2 = "englanti";
-        selectLanguagesTest();
-  } else if (finSwe.checked == true) {
-        lang1 = suomi;
-        lang2 = svenska;
-        header1 = "suomi";
-        header2 = "ruotsi";
-        return selectLanguagesTest();
-  } else if (engFin.checked == true) {
-        lang1 = english;
-        lang2 = suomi;
-        header1 = "englanti";
-        header2 = "suomi";
-        return selectLanguagesTest();
-  } else if(sweFin.checked == true) {
-        lang1 = svenska;
-        lang2 = suomi;
-        header1 = "ruotsi";
-        header2 = "suomi";
-        return selectLanguagesTest();
-  } else if(engSwe.checked == true) {
-        lang1 = english;
-        lang2 = svenska;
-        header1 = "englanti";
-        header2 = "ruotsi";
-        return selectLanguagesTest();
-  }
-}
-*/
 
-// If I used parameters, I probably don't need to repeat the same code over and over... Function runs when user selects "Finnish - English" button.
-function selectFinEng() {
-    lang1 = suomi;
-    lang2 = english;
-    header1 = "suomi";
-    header2 = "englanti";
-    return selectLanguagesTest();
-}
-
-// DO NOT DELETE, IT IS NOT UNIMPORTANT DESPITE OF THE NAME. Btw change the name for the final version.
-function selectLanguagesTest() {
+// Final language selection function.
+function selectLanguages(lang1, lang2, header1, header2) {
     let text = "<table><tr><th>" + header1 + "</th><th>" + header2 + "</th></tr>";
     for (var i = 0; i < words.length; i++) {
         text += "<tr><td>" + lang1[i] + "</td><td> " + lang2[i] + "</td></tr>";
     }
     text += "</table>"
     document.getElementById("outputAreaGetAll").innerHTML = text;
-    
-    // Unchecks radio buttons. Tbh I don't need the radio button uncheck anymore.
-    //uncheckOptions();
-   // console.log(selectedLanguage1);
 }
-
-/* Don't think I need this anymore since I dropped the radio buttons.
-function uncheckOptions() {
-    
-    finSwe.checked = false;
-    finEng.checked = false;
-    engFin.checked = false;
-    sweFin.checked = false;
-    engSwe.checked = false;
-    
-}
-*/
